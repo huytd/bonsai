1e0f107df3edc10240738d4213ba8a6844803da5
Huy (Henry) Tran <huytran@appformix.com>
2016-02-04 11:44:58 -0800

    Update env_setup.sh

--- env_setup.sh
+++ env_setup.sh
@@ -12,14 +12,14 @@ echo "Checking for PostgreSQL..."
 POSTGRES_PATH=`which initdb`
 if ["$POSTGRES_PATH" == ""]; then
 	echo "Installing PostgreSQL..."
-	brew install postgres
-	&& initdb /usr/local/var/postgres
-	&& mkdir -p ~/Library/LaunchAgents
-	&& ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
-	&& launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
-	&& createdb bonsai_development
-	&& createdb bonsai_production
-	&& createdb bonsai_test
+	brew install postgres && 
+	initdb /usr/local/var/postgres && 
+	mkdir -p ~/Library/LaunchAgents && 
+	ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents && 
+	launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist && 
+	createdb bonsai_development && 
+	createdb bonsai_production && 
+	createdb bonsai_test
 fi
 echo "OK!"
 
@@ -27,8 +27,7 @@ echo "Checking for rails..."
 RAILS_PATH=`which rails`
 if ["$RAILS_PATH" == ""]; then
 	echo "Installing rails..."
-	gem install rails
-	&& gem install pg -- --with-pg-config=/usr/local/bin/pg_config
+	gem install rails && gem install pg -- --with-pg-config=/usr/local/bin/pg_config
 fi
 echo "OK!"
 
