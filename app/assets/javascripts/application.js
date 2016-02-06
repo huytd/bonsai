b7a45d9f21f2849fe3a52270208c5dd9c3bf82d9
Huy (Henry) Tran <huytran@appformix.com>
2016-02-06 00:01:07 -0800

    Layout prototype and install heroku integration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap-sprockets
+//= require holder
 //= require_tree .
+
+$(document).bind('page:change', function(){
+    Holder.run();
+});
